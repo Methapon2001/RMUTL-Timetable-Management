699df511c50d0c551b07e37fa1da6f60f650cf63
Methapon2001 <dark.methapon13@gmail.com>
2023-01-08 19:18:37 +0700

    Navbar generator

--- client/src/lib/Navbar.svelte
+++ client/src/lib/Navbar.svelte
@@ -1,76 +1,53 @@
 <script>
+  import BoardIcon from "./Icons/BoardIcon.svelte";
+  import BuildingIcon from "./Icons/BuildingIcon.svelte";
+  import PersonIcon from "./Icons/PersonIcon.svelte";
+  import TimetableIcon from "./Icons/TimetableIcon.svelte";
+
   let path = window.location.pathname;
 
-  function handlePath(e) {
-    path = e.target.pathname;
-  }
+  let list = [
+    {
+      path: "/",
+      text: "Home",
+      icon: TimetableIcon,
+    },
+    {
+      path: "/instructor",
+      text: "Instructor",
+      icon: PersonIcon,
+    },
+    {
+      path: "/building",
+      text: "Building",
+      icon: BuildingIcon,
+    },
+    {
+      path: "/room",
+      text: "Room",
+      icon: BoardIcon,
+    },
+  ];
 </script>
 
-<div class="py-3 pl-3 pr-1">
-  <a
-    href="/"
-    class="nav-link {path == '/'
-      ? 'bg-slate-600'
-      : 'hover:bg-slate-600'}"
-    on:click={handlePath}
-  >
-    Timetable
-  </a>
-  <a
-    href="/instructor"
-    class="nav-link {path == '/instructor'
-      ? 'bg-slate-600'
-      : 'hover:bg-slate-600'}"
-    on:click={handlePath}
-  >
-    Instructor
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
-  <a href="#placeholder" class="nav-link" on:click={handlePath}>
-    Placeholder
-  </a>
+<div class="py-3 pl-3 pr-1 uppercase font-bold">
+  {#each list as page (page.text)}
+    <a
+      href={page.path}
+      class="nav-link {path == page.path && 'active'}"
+      on:click={() => (path = page.path)}
+    >
+      <div><svelte:component this={page.icon} /></div>
+      <span class="ml-2">{page.text}</span>
+    </a>
+  {/each}
 </div>
+
+<style type="text/postcss">
+  .nav-link:hover {
+    @apply bg-slate-600;
+  }
+  .active {
+    @apply bg-slate-600;
+  }
+</style>
